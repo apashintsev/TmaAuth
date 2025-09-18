1e09f91f750bc173b5565b29d023c3e215f1159e
apashintsev <av.pashintsev@gmail.com>
2025-09-18 13:40:43 +0300

    upgrade to .net9

--- Example/Program.cs
+++ Example/Program.cs
@@ -1,81 +1,137 @@
 using Example.Hubs;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Reflection;
 using System.Security.Claims;
 using TmaAuth;
 
-namespace Example
+namespace Example;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddCors(options =>
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            builder.Services.AddCors(options =>
+            options.AddPolicy("AllowAll", builder =>
             {
-                options.AddPolicy("AllowAll", builder =>
-                {
-                    builder.AllowAnyOrigin()
-                           .AllowAnyMethod()
-                           .AllowAnyHeader();
-                });
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
             });
+        });
 
-            // Add services to the container.
-            builder.Services.AddAuthentication()
-            .AddTelegramMiniAppToken(options =>
+        // Add services to the container.
+        builder.Services.AddAuthentication()
+        .AddTelegramMiniAppToken(options =>
+        {
+            options.BotToken = builder.Configuration["TelegramOptions:BotToken"];
+            options.Events = new TmaEvents
             {
-                options.BotToken = builder.Configuration["TelegramOptions:BotToken"];
-                options.Events = new TmaEvents
+                OnMessageReceived = context =>
                 {
-                    OnMessageReceived = context =>
+                    var accessToken = context.Request.Query["access_token"];
+                    if (!string.IsNullOrEmpty(accessToken) && context.Request.Path.StartsWithSegments("/balance"))
                     {
-                        var accessToken = context.Request.Query["access_token"];
-                        if (!string.IsNullOrEmpty(accessToken) && context.Request.Path.StartsWithSegments("/balance"))
-                        {
-                            context.InitDataRaw = accessToken;
-                        }
-                        return Task.CompletedTask;
+                        context.InitDataRaw = accessToken;
                     }
-                };
+                    return Task.CompletedTask;
+                }
+            };
+        });
+
+
+        builder.Services.AddAuthorization(options =>
+        {
+            options.AddPolicy(TmaPolicy.TmaUserPremiumPolicy, policy =>
+            {
+                policy.AddAuthenticationSchemes(TmaTokenDefaults.AuthenticationScheme);
+                policy.RequireClaim(ClaimTypes.NameIdentifier);
+                policy.RequireClaim(TmaClaim.IsPremium, "true");  // user must have premium account to access
+            });
+        });
+        builder.Services.AddSignalR();
+        builder.Services.AddScoped<IBalanceNotificationService, BalanceHub>();
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(s =>
+        {
+            s.SwaggerDoc("v1", new OpenApiInfo { Title = nameof(Assembly), Version = "1" });
+            s.ResolveConflictingActions(x => x.First());
+            s.AddSecurityDefinition("tma", new OpenApiSecurityScheme
+            {
+                Description = "TMA Authorization header using the scheme. Example: \"Authorization: TMA {token}\"",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "tma" // This should match the scheme used in the authentication handler
             });
 
-
-            builder.Services.AddAuthorization(options =>
+            var apiScheme = new OpenApiSecurityScheme
             {
-                options.AddPolicy(TmaPolicy.TmaUserPremiumPolicy, policy =>
+                Reference = new OpenApiReference
                 {
-                    policy.AddAuthenticationSchemes(TmaTokenDefaults.AuthenticationScheme);
-                    policy.RequireClaim(ClaimTypes.NameIdentifier);
-                    policy.RequireClaim(TmaClaim.IsPremium, "true");  // user must have premium account to access
-                });
-            });
-            builder.Services.AddSignalR();
-            builder.Services.AddScoped<IBalanceNotificationService, BalanceHub>();
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-            app.UseCors("AllowAll");
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "ApiKey"
+                },
+                In = ParameterLocation.Header,
+            };
+            // Add a security requirement for the TMA scheme
+            s.AddSecurityRequirement(new OpenApiSecurityRequirement()
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "tma"
+                        },
+                        In = ParameterLocation.Header,
+                    },
+                    new List<string>()
+                },
+            });
+            s.OperationFilter<AddAuthHeaderOperationFilter>();
+        });
 
-            app.UseHttpsRedirection();
+        var app = builder.Build();
+        app.UseCors("AllowAll");
 
-            app.UseAuthorization();
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
 
 
-            app.MapControllers();
-            app.MapHub<BalanceHub>("/balance");
+        app.MapControllers();
+        app.MapHub<BalanceHub>("/balance");
 
-            app.Run();
+        app.Run();
+    }
+}
+
+public class AddAuthHeaderOperationFilter : IOperationFilter
+{
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        if (operation.Parameters == null)
+            operation.Parameters = new List<OpenApiParameter>();
+
+        var authHeader = operation.Parameters.FirstOrDefault(p => p.Name == "Authorization");
+        if (authHeader != null)
+        {
+            authHeader.Description = "TMA {token}";
         }
     }
 }
